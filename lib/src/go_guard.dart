80ef96586aa557810b0afa2b104675eaa7bca59f
harkairt <32367206+harkairt@users.noreply.github.com>
2026-02-26 21:56:56 +0100

    chore: revert back to riverpod ^2.1.3

--- lib/src/go_guard.dart
+++ lib/src/go_guard.dart
@@ -1,5 +1,4 @@
-// Import ProviderListenable from misc.dart for Riverpod 3.0
-import 'package:hooks_riverpod/misc.dart' show ProviderListenable;
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 typedef Reader = T Function<T>(ProviderListenable<T> provider);
 
